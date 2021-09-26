db128b55e1d7d898a2363f214ab4780eb3338585
cnisidis <makeithappen@nisidis.com>
2021-09-26 11:57:54 +0300

    changed property names (removed identification from property group class names)

--- ui/__init__.py
+++ ui/__init__.py
@@ -1,10 +1,10 @@
 import bpy
 
 class Blend4vcamProperties(bpy.types.PropertyGroup):
-    blend4vcam_filepath             : bpy.props.StringProperty( 'Filepath', description="Define path for exported files", default="", subtype = "DIR_PATH", maxlen=1024)
-    blend4vcam_selected_only        : bpy.props.BoolProperty('Selected Only', description="Export only selected cameras", default = True )
-    blend4vcam_multiple_files       : bpy.props.BoolProperty('Multiple Files',description="If more than one cameras are selected, export them in separate files", default = True)
-    blend4vcam_write_to_textblock   : bpy.props.BoolProperty('Write To TextBlock', description="Write Camera Data in a Textblock instead of exporting it as a file (useful for debugging or packaging)", default = False)
+    blend4vcam_filepath             : bpy.props.StringProperty( 'filepath', description="Define path for exported files", default="", subtype = "DIR_PATH", maxlen=1024)
+    blend4vcam_selected_only        : bpy.props.BoolProperty('selected_only', description="Export only selected cameras", default = True )
+    blend4vcam_multiple_files       : bpy.props.BoolProperty('multiple_files',description="If more than one cameras are selected, export them in separate files", default = True)
+    blend4vcam_write_to_textblock   : bpy.props.BoolProperty('write_to_textblock', description="Write Camera Data in a Textblock instead of exporting it as a file (useful for debugging or packaging)", default = False)
  
 class Blend4v_PT_Panel(bpy.types.Panel):
     bl_idname = "Blend4v_PT_Panel"
@@ -23,13 +23,13 @@ class Blend4v_PT_Panel(bpy.types.Panel):
         layout = self.layout
         row = layout.row()
         subrow = layout.row(align=True)
-        subrow.prop(context.scene.blend4vcam, name="blend4vcam_filepath")
+        subrow.prop(context.scene.blend4vcam, name="filepath")
         subrow = layout.row(align=True)
-        subrow.prop(context.scene.blend4vcam, name="blend4vcam_selected_only")
+        subrow.prop(context.scene.blend4vcam, name="selected_only")
         subrow = layout.row(align=True)
-        subrow.prop(context.scene.blend4vcam, name="blend4vcam_multiple_files")
+        subrow.prop(context.scene.blend4vcam, name="multiple_files")
         subrow = layout.row(align=True)
-        subrow.prop(context.scene.blend4vcam, name="blend4vcam_write_to_textblock")
+        subrow.prop(context.scene.blend4vcam, name="write_to_textblock")
         subrow = layout.row(align=True)
         subrow.operator("blend4vcam.export")
         #layout.menu("OBJECT_MT_select_test", text="Presets", icon="SCENE")
